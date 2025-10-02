35e07fe080e629276bebbb527752eb5bb59330c7
dboss06 <131385319+dboss06@users.noreply.github.com>
2025-10-02 13:04:26 +0100

    First Task
    kjhjkl

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a figure");
+            double.TryParse(Console.ReadLine(), out double A);
+            Console.WriteLine("Enter a second figure");
+            double.TryParse(Console.ReadLine(), out double b);
+
+            Console.WriteLine("Enter a third figure");
+            double.TryParse(Console.ReadLine(), out double D);
+
+            Console.WriteLine("Enter a fourth figure");
+            double.TryParse(Console.ReadLine(), out double T);
+            Console.WriteLine("Enter a fifth figure");
+            double.TryParse(Console.ReadLine(), out double C);
+
+            double myresult = MyEqn(A, b, D, T, C);
+            Console.WriteLine(myresult);
+        }
+
+        static double MyEqn(double a, double b, double d, double t, double c)
+        {
+            double numerator = (Math.Sqrt((a * b) / (2.0 * d)) * 3) - Math.Sqrt(Math.Pow(Math.Sqrt(t * c) / (2.0 * d), 2));
+
+            double denominator = 4 * a * c;
+
+            double result = numerator / denominator;
+            return result;
+
+        }
+        
+    }
+
+
+
+}
